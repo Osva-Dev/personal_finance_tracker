54bef58b1aab7d2a4675f416afbd2d86bfd1597a
Osva-Dev <osvadev30@gmail.com>
2026-09-01 22:16:34 -0600

    first commit

---
+++ personal_finance_tracker.js
@@ -0,0 +1,9 @@
+/* 
+Proyecto: Personal Finance Tracker
+
+Crear un sistema de control financiero personal utilizando JavaScript puro y Node.js, cuya interfaz inicial será exclusivamente la consola/terminal.
+
+El sistema debe permitir al usuario crear y administrar categorías financieras. Cada categoría representará una fuente de ingresos, trabajo, negocio o cualquier actividad que el usuario quiera controlar de manera independiente.
+
+Cada categoría deberá tener su propio historial de movimientos, donde se puedan registrar entradas y salidas de dinero.
+*/
